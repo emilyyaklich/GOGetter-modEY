8e5e9f91c55eac89554006aecf0660bb1e7775f1
emilyyaklich <emily.yaklich@gmail.com>
2025-12-17 18:46:59 -0500

    add wrapper for running

---
+++ run_gg.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=run_GOgetter
+#SBATCH --partition=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --mem=100GB
+#SBATCH --time=48:00:00
+#SBATCH --output=run_gg%j.out
+#SBATCH --error=run_gg%j.err
+#SBATCH --mail-user=ely67071@uga.edu
+#SBATCH --mail-type=ALL
+
+
+module load  BLAST+/2.13.0-gompi-2022a
+module load Miniforge3
+
+source activate /home/ely67071/myenvs/sunflower_stress
+
+input_dir=$1
+
+#makeblastdb -in "${input_dir}/Araport11_pep_20250411_representative_gene_model" -dbtype prot
+
+
+
+bash GOgetter.sh -i ha412_transcript_b3.fasta -D "${input_dir}/ATH_GO_GOSLIM.txt" -d "${input_dir}/Araport11_pep_20250411_representative_gene_model"
