8694006220d6b6261522d41a2830e0c7b3c08b38
emilyyaklich <emily.yaklich@gmail.com>
2025-12-18 10:01:38 -0500

    add different parsing mech

--- make_tables.py
+++ make_tables.py
@@ -22,8 +22,8 @@ best_hits_S = best_hits[['Subject']]
 best_hits_S.columns = ['ObjectName']
 
 # Read in the GOSlim database and add names to each column 
-GOSlim_DB = pd.read_table(SLIM_DB, header = None, sep='\t')
-GOSlim_DB.columns = ['locus', 'TAIRAccession', 'ObjectName', 'RelationshipType','GOTerm','GOID','TAIRKeywordID','Aspect','GOSlimTerm','EvidenceCode','EvidenceDescription','EvidenceWidth','Ref','Annotator','Date']
+GOSlim_DB = pd.read_table(SLIM_DB, sep='\t', comment='!')
+GOSlim_DB.columns = ['locus','TAIRAccession', 'ObjectName', 'RelationshipType','GOTerm','GOID','TAIRKeywordID','Aspect','GOSlimTerm','EvidenceCode','EvidenceDescription','EvidenceWidth','Ref','Annotator','Date']
 
 # Generate a new df with the locus information 
 best_hits_S_locus = pd.DataFrame(best_hits_S['ObjectName'].str.replace('\.[0-9]','', regex = True))
@@ -57,32 +57,34 @@ freqcounts_locus.to_csv(HITS + '.freqcounts-locus.tsv',sep='\t')
 
 # Below is code added by E Yaklich to create an output file with geneID and GO term mapping
 # create a copy to avoid SettingWithCopyWarning
-best_hits_copy = best_hits[['Query', 'Subject']].copy()
+best_hits = pd.read_table(HITS)
+#best_hits_copy = best_hits[['Query', 'Subject']].copy()
+
+best_hits_subset = best_hits[['Query', 'Subject']].copy()
 
 # just keep transcript ID
-best_hits_copy['Query'] = best_hits_copy['Query'].str.extract(r'([^\::]+)')
+best_hits_subset['Query'] = best_hits_subset['Query'].str.extract(r'([^\::]+)')
 
 
 # rename columns for clarity
-best_hits_S.columns = ['gene_name', 'TAIR_ID']
+best_hits_subset.columns = ['gene_name', 'TAIR_ID']
 
-# clean the 'TAIR_ID' column by removing version numbers (e.g., AT2G07760.1 -> AT2G07760)
+# clean the 'TAIR_ID' column by removing version numbers (e.g., AT2G07760.1 -> AT2G07760)....i.e. going from transcript to locus. This will remove any number of digits or letters, ex ID.t18 or ID.UORF1
 # this is needed to map to the GOSlim database
-best_hits_S['TAIR_ID'] = best_hits_S['TAIR_ID'].str.replace(r'\.\d+$', '', regex=True)
+best_hits_subset['TAIR_ID'] = best_hits_subset['TAIR_ID'].str.replace(r'\..*', '', regex=True)
 
 # subset the GOSlim database to include the TAIRAccession (Arabidopsis TAIR ID), the associated GO term and GOID, and Evidence Code
-GOSlim_DB_subset = GOSlim_DB[['TAIRAccession', 'GOID', 'GOTerm','EvidenceCode']].copy()  # make a copy to avoid SettingWithCopyWarning
-
+GOSlim_DB_subset = GOSlim_DB[['locus', 'GOID', 'GOTerm','EvidenceCode']].copy()  # make a copy to avoid SettingWithCopyWarning
 
 # Check example sample cleaned data from both files
 print("Sample Best Hits (TAIR IDs cleaned):")
-print(best_hits_copy.head())
+print(best_hits_subset.head())
 
 print("Sample GOSlim DB (TAIRAccession cleaned):")
 print(GOSlim_DB_subset.head())
 
-# merge based on cleaned-up 'TAIR_ID' (your genes) and 'TAIRAccession' (from GOSlim DB)
-GOSlim_merge_gene = best_hits_copy.merge(GOSlim_DB_subset, 'inner', left_on='TAIR_ID', right_on='TAIRAccession')
+# merge based on cleaned-up 'TAIR_ID' and 'locus' (from GOSlim DB)
+GOSlim_merge_gene = best_hits_subset.merge(GOSlim_DB_subset, 'inner', left_on='TAIR_ID', right_on='locus')
 
 # check the first few rows of the merged result to ensure there are matches
 print("Merged Data (first few rows):")
